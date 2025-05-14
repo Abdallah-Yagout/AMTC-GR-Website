344ae430a781574387dcb87593cdfdc2b65fb155
hasan yagout <yagouthasan3@gmail.com>
2025-05-14 21:43:16 +0300

    first commit

--- resources/views/news/view.blade.php
+++ resources/views/news/view.blade.php
@@ -75,7 +75,6 @@
                                     <svg class="ml-1 w-4 h-4 transition-transform duration-300 group-hover:translate-x-1 {{app()->getLocale()=='ar'?'rotate-180':''}}" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
                                     </svg>
-
                                 </a>
                             </div>
                         </article>

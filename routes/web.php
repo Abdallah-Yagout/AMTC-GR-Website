60f30aa332f458a8bb4cd902ffa2912d3a1c6053
Hasan Yagout <yagouthasan3@gmail.com>
2025-05-26 13:21:28 +0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,8 @@ Route::middleware([SetLocale::class])->group(function () {
     Route::get('/community', [\App\Http\Controllers\ForumController::class, 'index'])->name('forum.index');
     Route::get('/tournament', [\App\Http\Controllers\TournamentController::class, 'index'])->name('tournament.index');
     Route::get('/leaderboard', [\App\Http\Controllers\LeaderboardController::class, 'index'])->name('leaderboard.index');
-    Route::get('/tournament/apply/{id}', [\App\Http\Controllers\LeaderboardController::class, 'apply'])->name('tournament.apply');
-    Route::post('/tournament/submit', [\App\Http\Controllers\LeaderboardController::class, 'submit'])->name('tournament.submit');
+    Route::get('/tournament/apply/{id}', [\App\Http\Controllers\TournamentController::class, 'apply'])->name('tournament.apply');
+    Route::post('/tournament/submit', [\App\Http\Controllers\TournamentController::class, 'submit'])->name('tournament.submit');
     Route::get('/news', [\App\Http\Controllers\NewsController::class, 'index'])->name('news.index');
     Route::get('/news/view/{slug}', [\App\Http\Controllers\NewsController::class, 'view'])->name('news.view');
     Route::get('language/{locale}', [\App\Http\Controllers\HomeController::class, 'switchLanguage'])->name('language.switch');

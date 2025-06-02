8d0a49420c209c0390aeee23112af34fb4afb63c
hasan yagout <yagouthasan3@gmail.com>
2025-06-02 18:51:49 +0300

    add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
+        <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
@@ -12,14 +12,14 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        
+
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 
         <!-- Styles -->
         @livewireStyles
         <script src="{{ asset('js/jquery.js') }}"></script>
-        
+
     </head>
     <body class="{{ app()->getLocale() === 'ar' ? 'font-cairo' : 'font-changa' }}">
 
